72463d58fa26458ff22eee7f6a1e24a64cfa98d6
dice <dimitrijedimitrijevic93@gmail.com>
2019-12-20 12:32:21 +0100

    wip

--- lib/hotel_wonderland_web/controllers/admin/room_controller.ex
+++ lib/hotel_wonderland_web/controllers/admin/room_controller.ex
@@ -1,67 +1,68 @@
 defmodule HotelWonderlandWeb.Admin.AdminRoomController do
-    use HotelWonderlandWeb, :controller 
+  use HotelWonderlandWeb, :controller
 
-    alias HotelWonderland.Accounts
-    alias HotelWonderland.Accounts.Room
+  alias HotelWonderland.Accounts
+  alias HotelWonderland.Accounts.Room
 
-    def index(conn, _params) do
-        rooms = Accounts.list_available_rooms(:preload)
-        render(conn, "index.html", rooms: rooms)
-      end
-    
-      def index_all_rooms(conn, _params) do
-        rooms = Accounts.list_rooms()
-        render(conn, "index_all_rooms.html", rooms: rooms)
-      end
-    
-      def new(conn, _params) do
-        changeset = Accounts.change_room(%Room{})
-        render(conn, "new.html", changeset: changeset)
-      end
-    
-      def create(conn, %{"room" => room_params}) do
-        case Accounts.create_room(room_params) do
-          {:ok, room} ->
-            conn
-            |> put_flash(:info, "Room created successfully.")
-            |> redirect(to: Routes.room_path(conn, :show, room))
-    
-          {:error, %Ecto.Changeset{} = changeset} ->
-            render(conn, "new.html", changeset: changeset)
-        end
-      end
-    
-      def show(conn, %{"id" => id}) do
-        room = Accounts.get_room!(id)
-        render(conn, "show.html", room: room)
-      end
-    
-      def edit(conn, %{"id" => id}) do
-        room = Accounts.get_room!(id)
-        changeset = Accounts.change_room(room)
-        render(conn, "edit.html", room: room, changeset: changeset)
-      end
-    
-      def update(conn, %{"id" => id, "room" => room_params}) do
-        room = Accounts.get_room!(id)
-    
-        case Accounts.update_room(room, room_params) do
-          {:ok, room} ->
-            conn
-            |> put_flash(:info, "Room updated successfully.")
-            |> redirect(to: Routes.room_path(conn, :show, room))
-    
-          {:error, %Ecto.Changeset{} = changeset} ->
-            render(conn, "edit.html", room: room, changeset: changeset)
-        end
-      end
-    
-      def delete(conn, %{"id" => id}) do
-        room = Accounts.get_room!(id)
-        {:ok, _room} = Accounts.delete_room(room)
-    
+  def index(conn, _params) do
+    rooms_from_db = Accounts.list_rooms()
+    rooms = Enum.reverse(rooms_from_db)
+    render(conn, "index.html", rooms: rooms)
+  end
+
+  def show_reservations(conn, _params) do
+    rooms = Accounts.list_reservations(:preload)
+    render(conn, "show-reservations.html", rooms)
+  end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_room(%Room{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"room" => room_params}) do
+    case Accounts.create_room(room_params) do
+      {:ok, room} ->
         conn
-        |> put_flash(:info, "Room deleted successfully.")
-        |> redirect(to: Routes.room_path(conn, :index_all_rooms))
-      end
+        |> put_flash(:info, "Room created successfully.")
+        |> redirect(to: Routes.admin_room_path(conn, :show, room))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    room = Accounts.get_room!(id)
+    render(conn, "show.html", room: room)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    room = Accounts.get_room!(id)
+    changeset = Accounts.change_room(room)
+    render(conn, "edit.html", room: room, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "room" => room_params}) do
+    room = Accounts.get_room!(id)
+
+    case Accounts.update_room(room, room_params) do
+      {:ok, room} ->
+        conn
+        |> put_flash(:info, "Room updated successfully.")
+        |> redirect(to: Routes.admin_room_path(conn, :show, room))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", room: room, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    room = Accounts.get_room!(id)
+    {:ok, _room} = Accounts.delete_room(room)
+
+    conn
+    |> put_flash(:info, "Room deleted successfully.")
+    |> redirect(to: Routes.admin_room_path(conn, :index))
+  end
 end
